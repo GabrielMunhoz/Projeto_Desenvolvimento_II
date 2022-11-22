eeab82dc03179e58409b603ed70175a6e1b7b18d
gabrielmunhoz <gabriell_munhoz@icloud.com>
2022-11-22 20:27:34 -0300

    PLSBK-50 - Added search players with pipe

---
+++ PlayersBook.Web/ClientApp/src/app/customPipe/search-players.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IPlayer } from '../models/Player/IPlayer';
+
+@Pipe({
+  name: 'searchPlayers'
+})
+export class SearchPlayersPipe implements PipeTransform {
+
+  transform(playersList: IPlayer[], valor?: string): IPlayer[] {
+    const nickName = valor ? valor : "";
+    console.log("entrou aqui")
+    console.log(nickName);
+    return playersList.filter(
+      (player) => 
+        player.nickname.toLocaleLowerCase().includes(nickName.toLocaleLowerCase())
+    );
+  }
+
+}
